dff96b3b5ce782dc911755fe14880654cf60a8d0
venkatesh-buddy <venkatesh88a@gmail.com>
2019-10-07 21:03:04 +0530

    Currency Exchange data sql

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT into currency_exchange(id,currency_from, currency_to,conversion_value,port)
+values(1000,'USD','INR', 75,0);
+INSERT into currency_exchange(id,currency_from, currency_to,conversion_value,port)
+values(1001,'EUR','INR', 95,0);
+INSERT into currency_exchange(id,currency_from, currency_to,conversion_value,port)
+values(1002,'KWT','INR', 280,0);
