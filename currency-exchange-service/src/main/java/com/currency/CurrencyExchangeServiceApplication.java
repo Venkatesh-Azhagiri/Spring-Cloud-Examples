581fc61006bf0bc75863074e91c28be39541efb8
venkatesh-buddy <venkatesh88a@gmail.com>
2019-10-07 21:01:13 +0530

    Currency Exchange service

---
+++ currency-exchange-service/src/main/java/com/currency/CurrencyExchangeServiceApplication.java
@@ -0,0 +1,17 @@
+package com.currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.currency.repository")
+@EntityScan(basePackages="com.currency.entity")
+public class CurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+}
