91ceb45735e0ca5ffc3d5e10e13762d7600f28f4
venkatesh-buddy <venkatesh88a@gmail.com>
2019-10-20 14:36:44 +0530

    Unused imports removed

--- spring-cloud-config-client/src/main/java/com/example/demo/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/example/demo/SpringCloudConfigClientApplication.java
@@ -2,9 +2,9 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
