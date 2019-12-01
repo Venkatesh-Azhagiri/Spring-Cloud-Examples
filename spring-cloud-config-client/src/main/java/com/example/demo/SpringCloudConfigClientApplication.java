600f0e1355c28c0afdad9f2dd53574469a09b001
venkatesh-buddy <venkatesh88a@gmail.com>
2019-12-01 23:24:47 +0530

    Hystrix Fault Tolerance

--- spring-cloud-config-client/src/main/java/com/example/demo/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/example/demo/SpringCloudConfigClientApplication.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @SpringBootApplication
+@EnableHystrix
 public class SpringCloudConfigClientApplication {
 
 	public static void main(String[] args) {
@@ -17,6 +21,9 @@ public class SpringCloudConfigClientApplication {
 		//POST: http://localhost:7777/actuator/refresh
 		//Will refresh all instance
 		//POST: http://localhost:7777/actuator/bus-refresh
+		
+		//Hystrix
+		//http://localhost:7777/propHystrix
 		SpringApplication.run(SpringCloudConfigClientApplication.class, args);
 	}
 
@@ -39,6 +46,18 @@ class SpringCloudClientConfigProp{
 	@RequestMapping(value="/prop")
 	public String getConfigProps(){
 		return this.prop+"::url::"+url+"::profile::"+profile;
+		
+	}
+	
+	@GetMapping(value="/propHystrix")
+	@HystrixCommand(fallbackMethod = "fallBackHystrix")
+	public String getConfigPropsHystrix(){
+		//return this.prop+"::url::"+url+"::profile::"+profile;
+		throw new RuntimeException("Fault Tolerance");		
+	}
+	
+	public String fallBackHystrix(){
+		return "This is default hystrix fault tolerance method";
 		
 	}
 	
